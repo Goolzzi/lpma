8d8815d43ebcf7e98806b1ad2a21761164ac0bb3
gnun <gnun.ulikhanyan@gmail.com>
2017-12-25 14:41:53 +0400

    configure contentfull

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `mp4fj9fzt99q`,
+        accessToken: `8eb674edd26e48fbd2b7d522a456cc75b4101451b7d070dceb92a0c4bad4991c`
+      }
+    }
+  ]
 };
