af9a07c720b0a19de216c7e1b74272d882a56ea4
gnun <gnunua@gmail.com>
2018-01-11 17:08:51 +0400

    testimonials responsive image handling

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -33,10 +33,18 @@ const Testimonials = ({title, testimonial1, testimonial2}) => (
     </section>
     <section className="hero exhibits">
       <div className="columns is-gapless">
-        {testimonial2.map(({id, image, body}) => (
+        {testimonial2.map(({id, image: {file}, body}) => (
           <div key={id} className="column is-4 is-12-mobile exhibit-item">
             <div className="image is-16by9">
-              <img src={image.file.url} alt="exhibit" />
+              <img
+                srcSet={`
+                ${file.url}?w=1140 1140w,
+                ${file.url}?w=960 960w,
+                ${file.url}?w=480 480w
+                `}
+                src={`${file.url}?w=960`}
+                alt="exhibit"
+              />
             </div>
             <p
               dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.html}}
