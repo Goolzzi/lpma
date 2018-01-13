492d7ba26f1a4df931b50057ff1f259f5c8aba84
gnun <gnunua@gmail.com>
2018-01-13 23:59:16 +0400

    format

--- src/components/TopColumns/index.js
+++ src/components/TopColumns/index.js
@@ -7,12 +7,15 @@ const TopColumns = ({copyrightHTML}) => (
     <div className="columns is-multiline">
       <div className="column is-12-mobile top-column">
         <p>
-          Leading Property Managers Association is a community of real estate professionals who seek to be extraordinary.
+          Leading Property Managers Association is a community of real estate
+          professionals who seek to be extraordinary.
         </p>
       </div>
       <div className="column is-12-mobile top-column">
         <p>
-          LPMA membership connects you to a network of real estate professionals and influential thought leaders and access to industry leading events, resources and people.
+          LPMA membership connects you to a network of real estate professionals
+          and influential thought leaders and access to industry leading events,
+          resources and people.
         </p>
       </div>
     </div>
