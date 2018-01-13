72fba112e41fa6318d2879c58f3c2308abee26d8
gnun <gnunua@gmail.com>
2018-01-14 00:36:58 +0400

    implement acquisition bage bottom jumbotron

--- src/components/BottomJumbotron/index.js
+++ src/components/BottomJumbotron/index.js
@@ -1,23 +1,42 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
 import "./styles.scss";
 
-const BottomJumbotron = () => (
-  <section className="hero bottom">
-    <img src={require("./../../assets/images/Audience (43).png")} />
+const propTypes = {
+  acquisitionJumbotron: PropTypes.array.isRequired,
+};
 
-    <section className="section wrapper">
-      <div className="columns">
-        <div className="column is-6 is-12-mobile">
-          <p>The leading community for leading property professionals</p>
-        </div>
-        <div className="column is-6 is-12-mobile">
-          <div className="has-text-right">
-            <button className="btn primary">Join the LPMA</button>
+const BottomJumbotron = ({acquisitionJumbotron}) => {
+  const jumbotron = acquisitionJumbotron[0];
+  const {
+    background: {resolutions: {src, srcSet}},
+    title: {title},
+    joinLink,
+  } = jumbotron;
+  return (
+    <section className="hero bottom">
+      <dev className="image is-16by9">
+        <img src={src} srcSet={srcSet} alt="acquisition bootom Jumbotron" />
+      </dev>
+      <section className="section wrapper">
+        <div className="columns">
+          <div className="column is-6 is-12-mobile">
+            <p>{title}</p>
+          </div>
+          <div className="column is-6 is-12-mobile">
+            <div className="has-text-right">
+              <Link to={joinLink.to}>
+                <button className="btn primary">{joinLink.name}</button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </section>
-  </section>
-);
+  );
+};
+
+BottomJumbotron.propTypes = propTypes;
 
 export default BottomJumbotron;
