f53c0bf5385ec23f100bda9910124cf1ea6e8e6a
gnun <gnunua@gmail.com>
2018-01-16 17:46:18 +0400

    implement resouces

--- src/components/Resources/index.js
+++ src/components/Resources/index.js
@@ -3,11 +3,51 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import "./styles.scss";
 
-const propTypes = {
-  info: PropTypes.array.isRequired,
+const ResurceItem = props => {
+  const {
+    title,
+    downloadLink,
+    orderLink,
+    description: {childMarkdownRemark},
+    image,
+  } = props;
+
+  return (
+    <div className="column is-4-desktop is-6-tablet is-12-mobile resource-item">
+      <h3>{title}</h3>
+      <p dangerouslySetInnerHTML={{__html: childMarkdownRemark.html}} />
+      {image ? (
+        <div className="image is-16by9">
+          <img
+            src={image.resolutions.src}
+            srcSet={image.resolutions.srcSet}
+            alt=""
+          />
+        </div>
+      ) : (
+        <React.Fragment />
+      )}
+      {orderLink ? (
+        <Link to={orderLink.to}>
+          <button className="btn secondary halfwidth">{orderLink.name}</button>
+        </Link>
+      ) : (
+        <a href={downloadLink.resourseHref} download>
+          <button className="btn secondary halfwidth">
+            {downloadLink.name}
+          </button>
+        </a>
+      )}
+    </div>
+  );
 };
 
-const Resources = ({info}) => {
+const propTypes = {
+  info: PropTypes.array.isRequired,
+  edges: PropTypes.array.isRequired,
+};
+
+const Resources = ({info, edges}) => {
   const {
     childContentfulColumnTextRemarkContentTextNode: {childMarkdownRemark},
   } = info[0];
@@ -19,71 +59,7 @@ const Resources = ({info}) => {
         dangerouslySetInnerHTML={{__html: childMarkdownRemark.html}}
       />
       <div className="columns is-gapless is-multiline">
-        <div className="column is-4-desktop is-6-tablet is-12-mobile resource-item">
-          <h3>Free Window Stickers</h3>
-          <p>
-            Our popular, attention-grabbing stickers can help denote your agency
-            as an LPMA member to your patrons. Contact us below to set up a
-            delivery.
-          </p>
-          <div className="image is-16by9">
-            <img src={require("./../../assets/images/resource-1.png")} />
-          </div>
-          <button className="btn secondary halfwidth">Order</button>
-        </div>
-        <div className="column is-4-desktop is-6-tablet is-12-mobile resource-item">
-          <h3>Printer Ready Member Badge</h3>
-          <p>
-            Proudly display your LPMA difference. Insert LPMA branding on your
-            stationery and business cards to elevate your point of difference.
-          </p>
-          <div className="image is-16by9">
-            <img src={require("./../../assets/images/resource-2.png")} />
-          </div>
-          <button className="btn secondary halfwidth">Download</button>
-        </div>
-        <div className="column is-4-desktop is-6-tablet is-12-mobile resource-item">
-          <h3>Digital Member Badge</h3>
-          <p>
-            Add your LPMA membership distinction to your emails, social media
-            pages and website with our digital LPMA member badges.
-          </p>
-          <div className="image is-16by9">
-            <img src={require("./../../assets/images/resource-3.png")} />
-          </div>
-          <button className="btn secondary halfwidth">Download</button>
-        </div>
-        <div className="column is-4-desktop is-6-tablet is-12-mobile resource-item">
-          <h3>Listing Kit Insert</h3>
-          <p>
-            Our popular, attention-grabbing stickers can help denote your agency
-            as an LPMA member to your patrons. Contact us below to set up a
-            delivery.
-          </p>
-          <div className="image is-16by9">
-            <img src={require("./../../assets/images/resource-4.png")} />
-          </div>
-          <button className="btn secondary halfwidth">Order</button>
-        </div>
-        <div className="column is-4-desktop is-6-tablet is-12-mobile resource-item">
-          <h3>How to use the LPMA brand</h3>
-          <p>
-            Thinking of implementing the LPMA logo on your site, social media or
-            letterhead? Follow this quick guide for proper use cases.
-          </p>
-          <div className="image is-16by9">
-            <img src={require("./../../assets/images/resource-5.png")} />
-          </div>
-          <button className="btn secondary halfwidth">Order</button>
-        </div>
-        <div className="column is-4-desktop is-6-tablet is-12-mobile resource-item">
-          <h3>Need Further Help?</h3>
-          <p>
-            If you have any questions about this brand kit or how to use the
-            LPMA member badge please contact us.
-          </p>
-          <button className="btn secondary halfwidth">Contact Us</button>
-        </div>
+        {edges.map(({node}) => <ResurceItem key={node.id} {...node} />)}
       </div>
     </section>
   );
