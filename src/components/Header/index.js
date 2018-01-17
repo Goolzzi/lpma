eb243de8a037a5087b7a110d041b0999783b3b67
Gnun Ulikhanyan <gnun.ulikhanyan@gmail.com>
2018-01-17 23:32:29 +0400

    mobile viewport fixes (#14)
    * Footer (#3)
    
    * footer in progress
    
    * footer in progress
    
    * finished footer
    
    * format
    
    * fonts urls resolves
    
    * Member benefits (#4)
    
    * footer in progress
    
    * footer in progress
    
    * finished footer
    
    * format
    
    * add member benefits
    
    * call format
    
    * add line, some fixing
    
    * detach redundant scss imports
    
    * Acquisition (#5)
    
    implemented landing page
    
    * add missing fonts
    
    * implement memeber benefit section
    
    * implement site header
    
    * implement site layout footer
    
    * implement testimonials section
    
    * testimonials responsive image handling
    
    * Resources page (#6)
    
    * added resources page markup
    
    * added events page markup
    
    * fix top component
    
    * update gitignore
    
    * improve contentful image qraphql query
    
    * implement top jumbotron
    
    * Pages (#8)
    
    * add join page
    
    * change placeholder color
    
    * fixing on design
    
    * fix conflicts dev into pages
    
    * format
    
    * implement acquisition bage bottom jumbotron
    
    * minor fixes
    
    * implement acquisition page top columns info
    
    * refactor event page templating
    
    * implement upcomming events
    
    * add events page top and bottom jumbotrons
    
    * add resourses page top and bottom jumbotrons
    
    * add resources page top info remark
    
    * Pixel perfect (#9)
    
    * pixel perfect
    
    * minor changes
    
    * implement resouces
    
    * filter lpma resources
    
    * add audience
    
    * remove redundant assets
    
    * add join jumbotorn, all jumbotrons orders fix, code format, code cleanup
    
    * remove redundant components
    
    * query fixes
    
    * fixed testimonilas component production issue, resources query fixes
    
    * mobile view port styling fixes (#13)
    
    * fix footer and exhibits
    
    * add burger
    
    * implement nav menu on mobile viewport

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,25 +8,44 @@ const propTypes = {
   logo: PropTypes.object.isRequired,
 };
 
-const Header = ({topmenu, logo: {file}}) => (
-  <nav className="navbar">
-    <div className="navbar-brand">
-      <Link className="navbar-item" to={"/"}>
-        <img src={file.url} alt={file.fileName} />
-      </Link>
-      <button className="button navbar-burger" />
-    </div>
-    <div className="navbar-menu">
-      <div className="navbar-end">
-        {topmenu.map(({id, to, name}) => (
-          <Link className="navbar-item" key={id} to={to}>
-            {name}
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false,
+    };
+  }
+
+  render() {
+    const {topmenu, logo: {file}} = this.props;
+    const {isActive} = this.state;
+    return (
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link className="navbar-item" to={"/"}>
+            <img src={file.url} alt={file.fileName} />
           </Link>
-        ))}
-      </div>
-    </div>
-  </nav>
-);
+          <button
+            onClick={() => this.setState({isActive: !this.state.isActive})}
+            className={`button navbar-burger ${isActive ? "is-active" : ""}`}>
+            <span />
+            <span />
+            <span />
+          </button>
+        </div>
+        <div className={`navbar-menu ${isActive ? "is-active" : ""}`}>
+          <div className="navbar-end">
+            {topmenu.map(({id, to, name}) => (
+              <Link className="navbar-item" key={id} to={to}>
+                {name}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
 
 Header.propTypes = propTypes;
 
