85075010da37737a91eb9b73fb0aff3d5865b854
Gnun Ulikhanyan <gnun.ulikhanyan@gmail.com>
2018-01-22 17:37:26 +0400

    test 2

--- gatsby/createPages.js
+++ gatsby/createPages.js
@@ -8,4 +8,18 @@ module.exports = ({boundActionCreators}) => {
     redirectInBrowser: true,
     toPath: `https://halo-identity.lpma.com.au/users/sign_in`,
   });
+  createRedirect({
+    fromPath: `/login`,
+    isPermanent: true,
+    redirectInBrowser: true,
+    toPath: `https://halo-identity.lpma.com.au/users/sign_in`,
+  });
+  
+  createRedirect({
+    fromPath: `/membership`,
+    isPermanent: true,
+    redirectInBrowser: false,
+    toPath: `https://halo-identity.lpma.com.au/users/sign_in`,
+  });
+  
 };
