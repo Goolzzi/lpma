d9a00e77bfccc1daaa61361838823b94776d4c04
gnun <gnunua@gmail.com>
2018-01-31 22:04:30 +0400

    implement join form

--- src/components/Join/index.js
+++ src/components/Join/index.js
@@ -2,84 +2,100 @@ import React from "react";
 import Link from "gatsby-link";
 import "./styles.scss";
 
-function createMarkup() {
-  return {
-    __html: `
-    <form name="contact" netlify>
-  <p>
-    <label>Your Name: <input type="text" name="name"></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email"></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-    `,
-  };
-}
+class Join extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.formRef = null;
+  }
 
-const Join = () => (
-  <section className="section join">
-    <div className="columns is-gapless">
-      <div className="column is-8 join-wrapper">
-        <div className="join-cont">
-          <h2>Join</h2>
+  componentDidMount() {
+    if (this.formRef) {
+      this.formRef.setAttribute("data-netlify", "true");
+    }
+  }
 
-          <div dangerouslySetInnerHTML={createMarkup()} />
+  render() {
+    return (
+      <section className="section join">
+        <div className="columns is-gapless">
+          <div className="column is-8 join-wrapper">
+            <div className="join-cont">
+              <h2>Join</h2>
+              <p>
+                You&apos;re taking your first step towards growing your network.
+              </p>
+              <div className="columns">
+                <div className="column is-7">
+                  <form
+                    ref={form => {
+                      this.formRef = form;
+                    }}
+                    name="join">
+                    <input
+                      type="text"
+                      className="inp"
+                      name="First Name"
+                      placeholder="FirstName"
+                    />
+                    <input
+                      type="text"
+                      className="inp"
+                      name="Last Name"
+                      placeholder="LastName"
+                    />
+                    <input
+                      type="text"
+                      className="inp"
+                      name="Agency Name"
+                      placeholder="AgencyName"
+                    />
+                    <input
+                      type="text"
+                      className="inp"
+                      name="Email"
+                      placeholder="Email"
+                    />
+                    <input
+                      type="text"
+                      className="inp"
+                      name="Contact number"
+                      placeholder="ContactNumber"
+                    />
+                    <button type="submit" className="btn primary halfwidth">
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
 
-          <p>
-            You&apos;re taking your first step towards growing your network.
-          </p>
-          <div className="columns">
-            <div className="column is-7">
-              <form name="contact" netlify>
-                <input type="text" className="inp" placeholder="First Name" />
-                <input type="text" className="inp" placeholder="Last Name" />
-                <input type="text" className="inp" placeholder="Agency Name" />
-                <input type="text" className="inp" placeholder="Email" />
-                <input
-                  type="text"
-                  className="inp"
-                  placeholder="Contact number"
-                />
-                <button type="submit" className="btn primary halfwidth">
-                  Submit
-                </button>
-              </form>
+          <div className="column is-4">
+            <div className="any-questions">
+              <h3>Any Questions?</h3>
+              <p>
+                Shoot us an email at
+                <Link to="mailto:support@lpma.com"> support@lpma.com </Link>
+              </p>
+              <p>
+                View our Membership
+                <Link to="http://www.lpma.com/membership-terms-and-conditions/">
+                  {" "}
+                  Terms and Conditions{" "}
+                </Link>
+                or
+                <Link to="https://www.lpma.com/privacy-policy">
+                  {" "}
+                  Privacy Policy
+                </Link>
+                .
+              </p>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="column is-4">
-        <div className="any-questions">
-          <h3>Any Questions?</h3>
-          <p>
-            Shoot us an email at
-            <Link to="mailto:support@lpma.com"> support@lpma.com </Link>
-          </p>
-          <p>
-            View our Membership
-            <Link to="http://www.lpma.com/membership-terms-and-conditions/">
-              {" "}
-              Terms and Conditions{" "}
-            </Link>
-            or
-            <Link to="https://www.lpma.com/privacy-policy">
-              {" "}
-              Privacy Policy
-            </Link>
-            .
-          </p>
-        </div>
-      </div>
-    </div>
-  </section>
-);
+      </section>
+    );
+  }
+}
 
 export default Join;
