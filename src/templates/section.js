960333bd236000cd7ed8e7a1d999eb6c736c2257
gnun <gnunua@gmail.com>
2018-02-06 20:13:33 +0400

    query section data

--- src/templates/section.js
+++ src/templates/section.js
@@ -1,22 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const propTypes = {};
+const propTypes = {
+  data: PropTypes.object.isReqiered,
+};
 
-const FoundrySection = props => (
-  <div>
-    <h1>{props.location.pathname}</h1>
-    <code>{JSON.stringify(props)}</code>
-  </div>
-);
+const FoundrySection = ({
+  data: {
+    contentfulFoundrySection: {title, slug, contentPartOne, contentPartTwo},
+  },
+}) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: contentPartOne.childMarkdownRemark.html,
+        }}
+      />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: contentPartTwo.childMarkdownRemark.html,
+        }}
+      />
+    </div>
+  );
+};
 
 FoundrySection.propTypes = propTypes;
 
 export default FoundrySection;
 
-// export const pageQuery = graphql`
-//   query FoundrySectionPageQuery {
-//     allContentfulResoursesJumbotron() {
-//     }
-//   }
-// `;
+export const pageQuery = graphql`
+  query FoundrySectionPageQuery($slug: String) {
+    contentfulFoundrySection(slug: {eq: $slug}) {
+      title
+      slug
+      contentPartOne {
+        childMarkdownRemark {
+          html
+        }
+      }
+      contentPartTwo {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`;
