29e94330faae7069fbdbc135418a3ee8284c1e94
sergey94 <sergey_p94@mail.ru>
2018-02-06 20:13:09 +0400

    add markdown wrapper

--- src/templates/section.js
+++ src/templates/section.js
@@ -1,12 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./styles.scss";
 
 const propTypes = {};
 
 const FoundrySection = props => (
-  <div>
-    <h1>{props.location.pathname}</h1>
-    <code>{JSON.stringify(props)}</code>
+  <div className="markdown">
+    {/*<h1>{props.location.pathname}</h1>
+    <code>{JSON.stringify(props)}</code>*/}
+
+    <h3>asdasd</h3>
+    <p>asd sdf sdf s</p>
+    <blockquote>sdf sdf sdf sdfsd fs</blockquote>
   </div>
 );
 
