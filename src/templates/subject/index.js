70fada0882d6e6ec3f57c7ea045a59a64facf65c
gnun <gnunua@gmail.com>
2018-02-09 01:21:06 +0400

    implement segments guides tab

--- src/templates/subject/index.js
+++ src/templates/subject/index.js
@@ -1,99 +1,98 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import FondryCard from "../components/FondryCard";
+import classNames from "classnames";
 import BreadCrumb from "../components/BreadCrumb";
+import FoundryCard from "../components/FondryCard";
+import "./styles.scss";
+import index from "../section/index";
 
-const propTypes = {
-  data: PropTypes.object.isReqiered,
-};
+const TabItem = ({tab, isActive, onClick, index}) => (
+  <li onClick={() => onClick(index)} className={classNames({active: isActive})}>
+    <span>{tab}</span>
+  </li>
+);
 
-const FoundrySubject = ({
-  data: {contentfulFoundrySubject: {title, content}},
-}) => {
-  return (
-    <section className="section template-page">
-      <BreadCrumb />
-      <div className="container wrapper-cont">
-        <div className="columns">
-          <div className="column">
-            <h1>{title}</h1>
+class FoundrySubject extends React.Component {
+  static propTypes = {
+    data: PropTypes.object.isReqiered,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTabIndex: 0,
+    };
+  }
+
+  handleTabClick = index => {
+    this.setState({activeTabIndex: index});
+  };
+
+  render() {
+    const {data: {contentfulFoundrySubject}} = this.props;
+    const {activeTabIndex} = this.state;
+    const {title, content, guideTypes} = contentfulFoundrySubject;
+    const tabCount = guideTypes ? guideTypes.tabs.length : 0;
+
+    return (
+      <section className="section template-page">
+        <BreadCrumb />
+        <div className="container wrapper-cont">
+          <div className="columns">
+            <div className="column">
+              <h1>{title}</h1>
+            </div>
           </div>
+          <div
+            className="markdown"
+            dangerouslySetInnerHTML={{__html: content.childMarkdownRemark.html}}
+          />
+          <div className="custom-tabs">
+            <ul>
+              {tabCount !== 0 &&
+                guideTypes.tabs.map((tab, index) => (
+                  <TabItem
+                    onClick={this.handleTabClick}
+                    key={index}
+                    tab={tab}
+                    index={index}
+                    isActive={activeTabIndex === index}
+                  />
+                ))}
+            </ul>
+          </div>
+
+          {tabCount !== 0 &&
+            guideTypes.tabs.map((tab, i) => {
+              return (
+                <div
+                  key={i}
+                  className={classNames("tab-content-wrapper content-hide", {
+                    "content-visible": activeTabIndex === i,
+                  })}>
+                  <div className="columns is-multiline">
+                    {tabCount !== 0 &&
+                      contentfulFoundrySubject[tab].map(
+                        ({id, title, excrept}) => {
+                          return (
+                            <FoundryCard
+                              key={id}
+                              title={title}
+                              content={excrept.childMarkdownRemark.excerpt}
+                              href={"/#"}
+                            />
+                          );
+                        },
+                      )}
+                  </div>
+                </div>
+              );
+            })}
         </div>
-
-        <div
-          className="markdown"
-          dangerouslySetInnerHTML={{__html: content.childMarkdownRemark.html}}
-        />
-
-        <div className="custom-tabs">
-          <ul>
-            <li>
-              <span>Test Pictures</span>
-            </li>
-            <li className="active">
-              <span>Music</span>
-            </li>
-            <li>
-              <span>Videos</span>
-            </li>
-            <li>
-              <span>Documents</span>
-            </li>
-          </ul>
-        </div>
-
-        <div className="columns is-multiline">
-          <div className="column is-6">
-            <a className="foundry-card-item">
-              <h3>asd</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat
-              </p>
-            </a>
-          </div>
-          <div className="column is-6">
-            <a className="foundry-card-item">
-              <h3>asd</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat
-              </p>
-            </a>
-          </div>
-          <div className="column is-6">
-            <a className="foundry-card-item">
-              <h3>asd</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat
-              </p>
-            </a>
-          </div>
-          <div className="column is-6">
-            <a className="foundry-card-item">
-              <h3>asd</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat
-              </p>
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-FoundrySubject.propTypes = propTypes;
+      </section>
+    );
+  }
+}
 
 export default FoundrySubject;
 
@@ -106,6 +105,29 @@ export const pageQuery = graphql`
       content {
         childMarkdownRemark {
           html
+        }
+      }
+      guideTypes {
+        tabs
+      }
+      guides {
+        id
+        title
+        type
+        excrept {
+          childMarkdownRemark {
+            excerpt
+          }
+        }
+      }
+      documents {
+        id
+        title
+        type
+        excrept {
+          childMarkdownRemark {
+            excerpt
+          }
         }
       }
     }
