b96a03f3ef0362b75f34e9690eb371cdf11a3066
gnun <gnunua@gmail.com>
2018-02-14 20:00:18 +0400

    implement guide step page right sidepar and markup

--- src/templates/step/index.js
+++ src/templates/step/index.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./styles.scss";
 
+//className="active"
+
 const Step = ({data: {contentfulFoundryGude, contentfulFoundryStep}}) => {
   return (
-    <div>
-      <code>{JSON.stringify(contentfulFoundryStep)}</code>
-      <hr />
-      <code>{JSON.stringify(contentfulFoundryGude)}</code>
-    </div>
+    <section className="section guides">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-3 steps-menu-wrapper">
+            <h3>Steps:</h3>
+            <ul className="steps-menu">
+              {contentfulFoundryGude.steps.map(({title, slug}) => (
+                <li key={slug}>
+                  <span>{title}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="column is-9 guide-content">
+            <h1>Asd dfg fgh ghj</h1>
+            <div className="markdown">Markdown here</div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
