6f928f8e4d90b3eec6aee9f7ee728a5224d48a30
sergey94 <sergey_p94@mail.ru>
2018-02-22 18:19:19 +0400

    fixed my-foundry colums

--- src/pages/my-foundry/index.js
+++ src/pages/my-foundry/index.js
@@ -41,73 +41,47 @@ const MyFoundryPage = () => (
         </div>
       </section>
     </section>
-    <FeedbackForm
-      feedbackParams={{
-        type: "fondry",
-        title: "My Foundry",
-        slug: "foundry",
-      }}
-    />
     <section className="section container foundry-columns">
       <div className="columns">
-        <div className="column is-4 column-item">
-          <Icon name="lightbulb-o" className="icon-style" />
-          <h3>LPMA Foundry</h3>
-          <p>
-            From strategy and growth, to profitability and high performing
-            teams, LPMA Foundry is a self-directed, online learning tool with
-            guides, case studies and blogs across a broad range of topics.
-          </p>
-          <button className="btn secondary with-radius-5 smaller smaller-text">
-            Login to Foundry
-          </button>
-          <button className="btn secondary with-radius-5 smaller smaller-text outlined transparent">
-            Learn more
-          </button>
-        </div>
-        <div className="column is-4 column-item">
-          <Icon name="comments-o" className="icon-style" />
-          <h3>Member Forums</h3>
-          <p>
-            Discuss topics relevant to business leadership using LPMA’s private,
-            Apmasphere online forums for Agency principals.
-          </p>
-          <button className="btn secondary with-radius-5 smaller smaller-text">
-            Login to Forums
-          </button>
-        </div>
-        <div className="column is-4 column-item">
-          <Icon name="list-ol" className="icon-style" />
-          <h3>Procedures, Letters & Checklists</h3>
-          <p>
-            Enable your team to deliver high quality and consistent customer
-            communications using LPMA’s huge database of letters and forms, all
-            inside Apmasphere.
-          </p>
-          <button className="btn secondary with-radius-5 smaller smaller-text">
-            Login to Database
-          </button>
-        </div>
-      </div>
-
-      <div className="columns">
-        <div className="column is-12">
-          <div className="thinking-of-joining">
-            <h3>Thinking of Joining LPMA?</h3>
+        <div className="column is-4">
+          <div className="column-item">
+            <h3>Title asd sdf</h3>
             <p>
-              LPMA membership connects you to a network of real estate
-              professionals and influential thought leaders and access to
-              industry leading events, resources and people.
+              Paragraph sdkjf skd fksdnf sjdfj sjd fjs dfsdf s djbfsbd fbs
+              dfsdfjsdfbsdb sdbf sdjff jj ssdf sj jj sjdf js djjs fjs dsf s sj
+              df
             </p>
-            <button className="btn secondary with-radius-5 smaller smaller-text">
-              Join LPMA
-            </button>
-            <button className="btn secondary with-radius-5 smaller smaller-text outlined transparent">
-              Learn more
-            </button>
+          </div>
+        </div>
+        <div className="column is-4">
+          <div className="column-item">
+            <h3>Title asd sdf</h3>
+            <p>
+              Paragraph sdkjf skd fksdnf sjdfj sjd fjs dfsdf s djbfsbd fbs
+              dfsdfjsdfbsdb sdbf sdjff jj ssdf sj jj sjdf js djjs fjs dsf s sj
+              df
+            </p>
+          </div>
+        </div>
+        <div className="column is-4">
+          <div className="column-item">
+            <h3>Title asd sdf</h3>
+            <p>
+              Paragraph sdkjf skd fksdnf sjdfj sjd fjs dfsdf s djbfsbd fbs
+              dfsdfjsdfbsdb sdbf sdjff jj ssdf sj jj sjdf js djjs fjs dsf s sj
+              df
+            </p>
           </div>
         </div>
       </div>
+
+      <FeedbackForm
+        feedbackParams={{
+          type: "fondry",
+          title: "My Foundry",
+          slug: "foundry",
+        }}
+      />
     </section>
   </div>
 );
