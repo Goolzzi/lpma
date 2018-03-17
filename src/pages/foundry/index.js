933c7e5c2987be19bf54a326bc5487558fb6b6e6
gnun <gnunua@gmail.com>
2018-03-17 16:11:23 +0400

    get user profile

--- src/pages/foundry/index.js
+++ src/pages/foundry/index.js
@@ -6,103 +6,120 @@ import {fisherYates} from "../../utils";
 import LPMALink from "../../utils/LPMALink";
 import auth from "../../Auth/Auth";
 
-const MyFoundryPage = props => {
-  const {
-    data: {
-      contentfulMyFoundryHeading: {title, greeting, background, cards},
-      allContentfulFoundryGuide: {edges},
-    },
-  } = props;
-  return (
-    <div>
-      <section className="section container foundry-heading" />
-      <section className="section foundry">
-        <div className="image-wrapper">
-          <img
-            src={background.resolutions.src}
-            srcSet={background.resolutions.srcSet}
-            alt="my foundry heading"
-          />
-        </div>
-        <section className="section cont">
-          <div className="container">
-            <div className="columns">
-              <div className="column is-4">
-                <div className="top-text">
-                  <p>{title}</p>
-                  <h2
-                    dangerouslySetInnerHTML={{
-                      __html: `${
-                        greeting.childMarkdownRemark.html
-                      } __username__`,
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="columns">
-              {cards.map(({id, title, content}) => (
-                <div key={id} className="column is-6">
-                  <div className="text">
-                    <h3>{title}</h3>
-                    <div
+class MyFoundryPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userProfile: null,
+    };
+  }
+
+  componentDidMount() {
+    if (auth.isAuthenticated()) {
+      auth.getProfile((error, profile) => {
+        if (error) {
+          console.log("User Profile Error! ", error);
+        } else {
+          this.setState({userProfile: profile});
+        }
+      });
+    }
+  }
+
+  render() {
+    const {
+      data: {
+        contentfulMyFoundryHeading: {title, greeting, background, cards},
+        allContentfulFoundryGuide: {edges},
+      },
+    } = this.props;
+    return (
+      <div>
+        <section className="section container foundry-heading" />
+        <section className="section foundry">
+          <div className="image-wrapper">
+            <img
+              src={background.resolutions.src}
+              srcSet={background.resolutions.srcSet}
+              alt="my foundry heading"
+            />
+          </div>
+          <section className="section cont">
+            <div className="container">
+              <div className="columns">
+                <div className="column is-4">
+                  <div className="top-text">
+                    <p>{title}</p>
+                    <h2
                       dangerouslySetInnerHTML={{
-                        __html: content.childMarkdownRemark.html,
+                        __html: `${greeting.childMarkdownRemark.html} ${this
+                          .state.userProfile &&
+                          this.state.userProfile.nickname}`,
                       }}
                     />
                   </div>
                 </div>
-              ))}
-            </div>
-          </div>
-        </section>
-      </section>
-      <section className="section container foundry-columns">
-        <div className="columns">
-          {fisherYates(edges, 3).map(
-            ({node: {id, title, slug, excerpt, foundrystep}}) => {
-              return (
-                <div key={id} className="column is-4">
-                  <LPMALink
-                    to={`foundry/${slug}/${
-                      fisherYates(foundrystep, 1)[0].slug
-                    }`}>
-                    <div className="column-item">
+              </div>
+              <div className="columns">
+                {cards.map(({id, title, content}) => (
+                  <div key={id} className="column is-6">
+                    <div className="text">
                       <h3>{title}</h3>
-                      <p
+                      <div
                         dangerouslySetInnerHTML={{
-                          __html: excerpt.childMarkdownRemark.excerpt,
+                          __html: content.childMarkdownRemark.html,
                         }}
                       />
                     </div>
-                  </LPMALink>
-                </div>
-              );
-            },
-          )}
-        </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+        </section>
+        <section className="section container foundry-columns">
+          <div className="columns">
+            {fisherYates(edges, 3).map(
+              ({node: {id, title, slug, excerpt, foundrystep}}) => {
+                return (
+                  <div key={id} className="column is-4">
+                    <LPMALink
+                      to={`foundry/${slug}/${
+                        fisherYates(foundrystep, 1)[0].slug
+                      }`}>
+                      <div className="column-item">
+                        <h3>{title}</h3>
+                        <p
+                          dangerouslySetInnerHTML={{
+                            __html: excerpt.childMarkdownRemark.excerpt,
+                          }}
+                        />
+                      </div>
+                    </LPMALink>
+                  </div>
+                );
+              },
+            )}
+          </div>
 
-        <FeedbackForm
-          feedbackParams={{
-            type: "fondry",
-            title: "My Foundry",
-            slug: "foundry",
-          }}
-        />
-      </section>
-    </div>
-  );
-};
+          <FeedbackForm
+            feedbackParams={{
+              type: "fondry",
+              title: "My Foundry",
+              slug: "foundry",
+            }}
+          />
+        </section>
+      </div>
+    );
+  }
+}
 
 MyFoundryPage.propTypes = {
   data: PropTypes.object.isRquiered,
 };
 
-const MyFoundryPageWithAuth = props => {
-  return <MyFoundryPage auth={auth} {...props} />;
-};
-
-export default MyFoundryPageWithAuth;
+export default MyFoundryPage;
 
 export const pageQuery = graphql`
   query MyFoundryPageQuery {
