8fb528ca7a1969722ee68e7d04d0868ce0240ce9
gnun <gnunua@gmail.com>
2018-03-19 00:47:22 +0400

    fixed browser dependency issue

--- src/Auth/index.js
+++ src/Auth/index.js
@@ -69,8 +69,10 @@ class Auth {
   isAuthenticated = () => {
     // Check whether the current time is past the
     // access token's expiry time
-    let expiresAt = JSON.parse(localStorage.getItem("expires_at"));
-    return new Date().getTime() < expiresAt;
+    if (typeof localStorage !== "undefined") {
+      let expiresAt = JSON.parse(localStorage.getItem("expires_at"));
+      return new Date().getTime() < expiresAt;
+    }
   };
 }
 
