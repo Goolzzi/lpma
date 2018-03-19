27e35fe682c1e15a57338af865798d641364e056
gnun <gnunua@gmail.com>
2018-03-19 17:32:25 +0400

    refactored events page

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,11 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
-import TopJumbotron from "../components/TopJumbotron";
-import BottomJumbotron from "../components/BottomJumbotron";
-import Events from "../components/Events";
+import Link from "gatsby-link";
+import TopJumbotron from "../../components/TopJumbotron";
+import BottomJumbotron from "../../components/BottomJumbotron";
+import "./styles.scss";
 
-const propTypes = {
-  data: PropTypes.object.isRequired,
+const Event = ({name, date, location, description, buyTicketsLink}) => (
+  <div className="columns is-gapless event-item">
+    <div className="column is-7 event-name">
+      <h3>{name}</h3>
+      <p
+        dangerouslySetInnerHTML={{__html: description.childMarkdownRemark.html}}
+      />
+    </div>
+    <div className="column is-2 event-date">
+      <h4>{date}</h4>
+      <p>{location}</p>
+    </div>
+    <div className="column is-3 event-button">
+      <Link {...buyTicketsLink}>
+        <button className="btn primary outlined">{buyTicketsLink.name}</button>
+      </Link>
+    </div>
+  </div>
+);
+
+const Events = ({title, events}) => (
+  <section className="section upcoming-events">
+    <h3>{title}</h3>
+    {events.map(({node}) => <Event key={node.id} {...node} />)}
+  </section>
+);
+
+Events.propTypes = {
+  title: PropTypes.string.isRequired,
+  events: PropTypes.array,
 };
 
 const EventsPage = ({
@@ -22,7 +51,9 @@ const EventsPage = ({
   </div>
 );
 
-EventsPage.propTypes = propTypes;
+EventsPage.propTypes = {
+  data: PropTypes.object.isRequired,
+};
 
 export default EventsPage;
 
