e2183923b70062ca3b3e0256ac5eedfa4e12d3d9
Goolzzi <goolzzijeon@gmail.com>
2018-03-24 11:27:57 +0800

    add menu bar status
    Signed-off-by: Goolzzi <goolzzijeon@gmail.com>

--- src/NewComponent/Header/Header.js
+++ src/NewComponent/Header/Header.js
@@ -1,25 +1,47 @@
 import React from "react";
 import imgLogo from "../../assets/images/NewDesign/Header/logo.svg";
+var menuClass = '';
+var lightBtnClass = '';
+var btnClass = '';
 
-const Header = (props) => (
-  <nav className="navbar  is-transparent header-wrapper">
-    <div className="navbar-brand">
-      <a className="navbar-item" href="#">
-        <img src={imgLogo} alt="this is logo" width="112" height="28" />
-      </a>
-    </div>
-    <div className="menu">
-      <ul>
-        <li><a>Home</a></li>
-        <li><a>Pricing</a></li>
-        <li><a>Blog</a></li>
-        <li><a>Events</a></li>
-      </ul>
-    </div>
-    <div className="navbar-btn-group">
-      <div className="navbar-item"><a className="button menu-btn">JOIN LPMA</a></div>
-      <div className="navbar-item"><a className="button menu-btn">Sign in</a></div>
-    </div>
-  </nav>
-);
+const Header = (props) => {
+  console.log(props.pageNumber);
+  const { pageNumber } = props;
+  
+  if (pageNumber > 1) {
+    menuClass = 'transition-down menu-2';
+    lightBtnClass = '';
+    btnClass = '';
+  }
+  // let menuClass = 'page-1';
+  // switch (pageNumber) {
+  //   case 0:
+  //     menuClass = 'page-1';
+  //     break;
+  //   case 2:
+  //     menuClass = menuClass + ' page-2';
+  //     break;
+  // }
+  return (
+    <nav className={`navbar  is-transparent header-wrapper`}>
+      <div className="navbar-brand">
+        <a className="navbar-item" href="#">
+          <img src={imgLogo} alt="this is logo" width="112" height="28" />
+        </a>
+      </div>
+      <div className={`menu ${menuClass}`}>
+        <ul>
+          <li><a>Home</a></li>
+          <li><a>Pricing</a></li>
+          <li><a>Blog</a></li>
+          <li><a>Events</a></li>
+        </ul>
+      </div>
+      <div className="navbar-btn-group">
+        <div className="navbar-item"><a className="button menu-btn">JOIN LPMA</a></div>
+        <div className="navbar-item"><a className="button menu-btn">Sign in</a></div>
+      </div>
+    </nav>
+  )
+};
 export default Header;
--- src/layouts/new-layout.js
+++ src/layouts/new-layout.js
@@ -3,13 +3,24 @@ import "bulma";
 import Header from "../NewComponent/Header";
 
 import "../styles/landing_mode.scss";
-const NewLayoutTemplate = props => {
-  const { children } = props;
-  return (
-    <div>
-      <Header />
-      <div>{children()}</div>
-    </div>
-  )
+
+class NewLayoutTemplate extends React.PureComponent {
+  constructor(props) {
+    super()
+
+    this.state = {
+      number: 0
+    }
+  }
+  render() {
+    const { children } = this.props;
+    return (
+      <div>
+        <Header pageNumber={this.state.number}/>
+        <div>{children({ ...this.props, onPageChange: (number) => this.setState({number})})}</div>
+      </div>
+    )
+  }
+  
 }
 export default NewLayoutTemplate;
