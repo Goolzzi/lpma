8a3b35cf9a90b7b9050b338d0b952b14ece7bc6d
tnarek99 <narektumanyan99@gmail.com>
2018-04-06 19:12:45 +0400

    Minor fix

--- src/pages/foundry/index.js
+++ src/pages/foundry/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Img from "gatsby-image";
+import YouTube from "react-youtube";
 import IRISAuth from "../../Auth/IRISAuth";
 import {fisherYates} from "../../utils";
 import "./styles.scss";
