c0f928e708a824d76774c118b1a6db6dae4b6f55
tnarek99 <narektumanyan99@gmail.com>
2018-04-09 19:25:27 +0400

    Figma Icons for Business Capabilities

--- src/components/specific/foundry/BCCard/index.js
+++ src/components/specific/foundry/BCCard/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "gatsby-link";
-import {Icon} from "react-fa";
+import "./styles.scss";
 
 const propTypes = {
   node: PropTypes.shape({
     name: PropTypes.string.isRequired,
-    iconName: PropTypes.string.isRequired,
+    icon: PropTypes.object.isRequired,
     slug: PropTypes.string.isRequired,
   }),
 };
 
-const BCCard = ({node: {name, iconName, slug}}) => (
+const BCCard = ({node: {name, icon: {resolutions: {src, srcSet}}, slug}}) => (
   <div className="column is-4 level-card-wrapper">
     <div className="level-card-item smaller has-text-centered">
+      <img className="icon" src={src} srcSet={srcSet} alt="" />
       <Link className="is-size-5 has-icon" to={`/foundry/${slug}`}>
-        <Icon name={iconName} />
         {name}
       </Link>
     </div>
