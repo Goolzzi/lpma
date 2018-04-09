b823d7fad0a9aab59f8e6131f26b8a96049968e9
gnun <gnunua@gmail.com>
2018-04-09 19:08:35 +0400

    code format

--- src/NewComponent/Header/Header.js
+++ src/NewComponent/Header/Header.js
@@ -3,7 +3,6 @@ import Link from "gatsby-link";
 import PropTypes from "prop-types";
 import IRISAuth from "../../Auth/IRISAuth";
 import imgLogo from "../../assets/images/NewDesign/Header/logo.svg";
-import auth from "../../Auth/auth";
 const menuPrimaryIndexes = [0, 1, 18];
 const secondaryIndexes = [2, 6, 10, 14];
 const primaryIndexes = [0, 1, 3, 4, 5, 7, 8, 9, 11, 12, 13, 15, 16, 17, 18];
