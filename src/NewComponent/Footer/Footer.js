a1e85207d69dc4d33e0b2816d1870b5778ef40c3
Goolzzi <goolzzijeon@gmail.com>
2018-04-12 11:52:47 +0800

    added mobile version
    Signed-off-by: Goolzzi <goolzzijeon@gmail.com>

--- src/NewComponent/Footer/Footer.js
+++ src/NewComponent/Footer/Footer.js
@@ -5,26 +5,32 @@ import {CSSTransition} from "react-transition-group";
 const Footer = ({footerIn}) => (
   <CSSTransition in={footerIn} timeout={1000} classNames="footer" unmountOnExit>
     <div className="footer-wrapper">
-      <span className="email-label">Get in touch:</span>
-      <span className="email-address">hello@lpma.com</span>
-      <span className="phone-aus">
-        <span role="img" aria-label="flag">
-          🇦🇺
-        </span>{" "}
-        AUS +61 9 146 0050
-      </span>
-      <span className="phone-nz">
-        <span role="img" aria-label="flag">
-          🇳🇿
-        </span>{" "}
-        NZ +64 9 886 0520
-      </span>
-      <span className="phone-us">
-        <span role="img" aria-label="flag">
-          🇺🇸
-        </span>{" "}
-        USA 1 3056 092 757
-      </span>
+      <div className="mobile-column">
+        <span className="email-us">
+          <span className="email-label">Get in touch:</span>
+          <span className="email-address">hello@lpma.com</span>
+        </span>
+        <span className="phone-aus">
+          <span role="img" aria-label="flag">
+            🇦🇺
+          </span>{" "}
+          AUS +61 9 146 0050
+        </span>
+      </div>
+      <div className="mobile-column">
+        <span className="phone-nz">
+          <span role="img" aria-label="flag">
+            🇳🇿
+          </span>{" "}
+          NZ +64 9 886 0520
+        </span>
+        <span className="phone-us">
+          <span role="img" aria-label="flag">
+            🇺🇸
+          </span>{" "}
+          USA 1 3056 092 757
+        </span>
+      </div>
     </div>
   </CSSTransition>
 );
