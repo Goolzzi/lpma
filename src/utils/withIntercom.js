be646cf090f88e159fdd96d2a6fc8c1a116aa6bd
tnarek99 <narektumanyan99@gmail.com>
2018-05-02 14:48:01 +0400

    Functionality for Intercom conversion

--- src/utils/withIntercom.js
+++ src/utils/withIntercom.js
@@ -1,10 +1,36 @@
 import React from "react";
 import noop from "lodash/noop";
+import IntercomClient from "intercom-client";
+import {v4} from "uuid";
 import {getComponentDisplayName} from "../utils";
+
+// constants
+const LEAD_TYPE = "lead";
 
 function withIntercom(WrappedComponent) {
   class WithIntercom extends React.Component {
+    constructor(props) {
+      super(props);
+      this.client = new IntercomClient.Client({token: v4()});
+    }
+
+    generateConversion = visitorId => ({
+      visitor: {user_id: visitorId},
+      type: LEAD_TYPE,
+    });
+
     getVisitorId = () => Intercom("getVisitorId"); // eslint-disable-line
+
+    convert = conversion =>
+      this.client.visitors.convert(conversion, response =>
+        console.log(response.body),
+      );
+
+    visitorToLead = () => {
+      const visitorId = this.getVisitorId();
+      const conversion = this.generateConversion(visitorId);
+      this.convert(conversion);
+    };
 
     render() {
       if (typeof Intercom === "undefined") {
