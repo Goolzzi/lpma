7002af620639634fb27665dd525a32774494b959
tnarek99 <narektumanyan99@gmail.com>
2018-05-03 14:34:17 +0400

    Minor dependency testing

--- netlify/functions/visitorToLead.js
+++ netlify/functions/visitorToLead.js
@@ -1,10 +1,8 @@
-// const Intercom = require("intercom-client");
+const Intercom = require("intercom-client");
 
 exports.handler = function(event, context, callback) {
   const accessToken = process.env.INTERCOM_ACCESS_TOKEN;
-  console.log("CONTEXT", context);
-  console.log("EVENT", event);
-  console.log("CALLBACK", callback);
+  console.log("INTERCOM", Intercom);
   callback(null, {
     statusCode: 300,
     context: JSON.stringify(context),
