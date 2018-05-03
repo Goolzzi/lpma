00a549e11433e113a42fa6bec7807eceeb670510
tnarek99 <narektumanyan99@gmail.com>
2018-05-03 14:12:20 +0400

    Minor logs addition

--- netlify/functions/visitorToLead.js
+++ netlify/functions/visitorToLead.js
@@ -2,6 +2,9 @@
 
 exports.handler = function(event, context, callback) {
   const accessToken = process.env.INTERCOM_ACCESS_TOKEN;
+  console.log("CONTEXT", context);
+  console.log("EVENT", event);
+  console.log("CALLBACK", callback);
   callback(null, {
     statusCode: 300,
     context: JSON.stringify(context),
