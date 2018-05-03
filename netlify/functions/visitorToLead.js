127108c7743cf9e749aa18587c314e58e8686bdb
tnarek99 <narektumanyan99@gmail.com>
2018-05-03 13:11:35 +0400

    Changed status code for testing

--- netlify/functions/visitorToLead.js
+++ netlify/functions/visitorToLead.js
@@ -3,7 +3,7 @@
 exports.handler = function(event, context, callback) {
   const accessToken = process.env.INTERCOM_ACCESS_TOKEN;
   callback(null, {
-    statusCode: 200,
+    statusCode: 300,
     context: JSON.stringify(context),
     at: accessToken,
   });
