13d4cdfd894d59ac908a2a5ab66741a9d712196b
tnarek99 <narektumanyan99@gmail.com>
2018-05-03 18:24:32 +0400

    Functions package changes

--- netlify/functions/visitorToLead.js
+++ netlify/functions/visitorToLead.js
@@ -1,8 +1,8 @@
-// const Intercom = require("intercom-client");
+const Intercom = require("intercom-client");
 
 exports.handler = function(event, context, callback) {
   const accessToken = process.env.INTERCOM_ACCESS_TOKEN;
-  console.log("INTERCOM");
+  console.log("INTERCOM", Intercom);
   callback(null, {
     statusCode: 300,
     context: JSON.stringify(context),
