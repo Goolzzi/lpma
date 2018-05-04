eb9fe2ab5a6ed2d6c114dccb8ae34810d4cfda1a
tnarek99 <narektumanyan99@gmail.com>
2018-05-04 16:17:26 +0400

    minor change

--- functions/visitorToLead.js
+++ functions/visitorToLead.js
@@ -1,14 +1,9 @@
-// const Intercom = require("./node_modules/intercom-client");
+const Intercom = require("./node_modules/intercom-client");
 const fs = require("fs");
 
 exports.handler = function(event, context, callback) {
   const accessToken = process.env.INTERCOM_ACCESS_TOKEN;
-  // console.log("INTERCOM", Intercom);
-  try {
-    console.log("FILES", fs.readdirSync("./node_modules"));
-  } catch (e) {
-    console.log("error!!!!", e);
-  }
+  console.log("INTERCOM", Intercom);
   callback(null, {
     statusCode: 200,
     context: JSON.stringify(context),
