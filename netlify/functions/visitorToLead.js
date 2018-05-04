818df838650dc1f5c23ea7f9537687ce90c544a3
tnarek99 <narektumanyan99@gmail.com>
2018-05-04 14:32:10 +0400

    minor dir log

--- netlify/functions/visitorToLead.js
+++ netlify/functions/visitorToLead.js
@@ -1,8 +1,10 @@
-const Intercom = require("./node_modules/intercom-client");
+// const Intercom = require("./node_modules/intercom-client");
+const fs = require("fs");
 
 exports.handler = function(event, context, callback) {
   const accessToken = process.env.INTERCOM_ACCESS_TOKEN;
-  console.log("INTERCOM", Intercom);
+  // console.log("INTERCOM", Intercom);
+  console.log("FILES", fs.readdirSync("."));
   callback(null, {
     statusCode: 300,
     context: JSON.stringify(context),
