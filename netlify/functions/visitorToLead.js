16ce594f0e8308383a0e848f9c1af74b4c7652f3
tnarek99 <narektumanyan99@gmail.com>
2018-05-04 15:15:30 +0400

    minor changes

--- netlify/functions/visitorToLead.js
+++ netlify/functions/visitorToLead.js
@@ -4,9 +4,13 @@ const fs = require("fs");
 exports.handler = function(event, context, callback) {
   const accessToken = process.env.INTERCOM_ACCESS_TOKEN;
   // console.log("INTERCOM", Intercom);
-  console.log("FILES", fs.readdirSync("./node_modules"));
+  try {
+    console.log("FILES", fs.readdirSync("./node_modules"));
+  } catch (e) {
+    console.log("error!!!!", e);
+  }
   callback(null, {
-    statusCode: 300,
+    statusCode: 200,
     context: JSON.stringify(context),
     at: accessToken,
   });
