e15e88549ffa6db9dc3f5fcc3e958a9c5572e017
tnarek99 <narektumanyan99@gmail.com>
2018-06-20 12:34:58 +0400

    Added 404 styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+const styles = {
+  container: {
+    marginTop: 40,
+    marginBottom: 67,
+    padding: 20,
+    display: "block",
+    maxWidth: 1348,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+};
+
 const NotFoundPage = () => (
-  <div>
-    <h1>{"NOT FOUND"}</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div style={styles.container}>
+    <h1>404</h1>
+    <h1>{"Houston we have a problem..."}</h1>
+    <p>{"We can't seem to find the page you are looking for."}</p>
   </div>
 );
 
