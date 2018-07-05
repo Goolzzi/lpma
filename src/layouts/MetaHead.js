164c60c1b3dfb656b67050c9cc4f56f7a5f845e4
James Sandri <james@studiopda.com.au>
2018-07-05 14:08:49 +1000

    og image

--- src/layouts/MetaHead.js
+++ src/layouts/MetaHead.js
@@ -8,6 +8,8 @@ const propTypes = {
   title: PropTypes.string.isRequired,
 };
 
+import ogImage from "../assets/images/og_image.jpg";
+
 const MetaHead = ({metaImage1200x630, metaImage1024x512, title}) => {
   return (
     <Helmet title={title}>
@@ -15,13 +17,13 @@ const MetaHead = ({metaImage1200x630, metaImage1024x512, title}) => {
         name="description"
         content="Leading Property Managers Association is a community of real estate professionals who seek to be extraordinary."
       />
-      <meta name="image" content={metaImage1200x630} />
+      <meta name="image" content={ogImage} />
       <meta itemProp="name" content="Leading Property Managers Association" />
       <meta
         itemProp="description"
         content="Leading Property Managers Association is a community of real estate professionals who seek to be extraordinary."
       />
-      <meta itemProp="image" content={metaImage1200x630} />
+      <meta itemProp="image" content={ogImage} />
       <meta name="twitter:card" content="summary" />
       <meta
         name="twitter:title"
@@ -32,13 +34,13 @@ const MetaHead = ({metaImage1200x630, metaImage1024x512, title}) => {
         content="Leading Property Managers Association is a community of real estate professionals who seek to be extraordinary."
       />
       <meta name="twitter:site" content="@lpmassoc" />
-      <meta name="twitter:image:src" content={metaImage1024x512} />
+      <meta name="twitter:image:src" content={ogImage} />
       <meta name="og:title" content="Leading Property Managers Association" />
       <meta
         name="og:description"
         content="Leading Property Managers Association is a community of real estate professionals who seek to be extraordinary."
       />
-      <meta name="og:image" content={metaImage1200x630} />
+      <meta name="og:image" content={ogImage} />
       <meta name="og:url" content="http://www.lpma.com/" />
       <meta
         name="og:site_name"
